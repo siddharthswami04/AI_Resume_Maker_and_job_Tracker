1a81c5d5bbde74ad9bd6b5380f8c432cc6ee2487
Siddharth <siddharthswami40@gmail.com>
2026-07-30 10:29:55 +0530

    Enhance app.py with Streamlit layout and job fetching
    Refactor app.py to improve structure and add Streamlit UI.

--- app.py
+++ app.py
@@ -1,61 +1,112 @@
+#==========LOAD MODULES========================
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
 import langchain
 from langchain.agents import create_agent
 from tavily import TavilyClient
-import pytesseract as py
+import pytesseract as pyt 
 import streamlit as st
 import os
 import time
 from PIL import Image
 import pandas as pd
 import numpy as np
+
+
+
+# To Show web-app: complete page layout
+st.set_page_config(layout="wide")
+
+# To Give Title
+st.title("AI RESUME GENERATOR")
+
+st.write("""This app helps user to build customized Professional
+Resume with Latest Job apply links""")
+
+st.image("BG.jpg")
+
+
+
+
+# ========API KEYS============# 
+# Step 3 API keys
 TAVILY_API_KEY =" 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"
 GROQ_API_KEY ="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"
 GOOGLE_API_KEY ="AQ.Ab8RN6JWhO0L-Z8NKtu2EFVic-ZICA3a9fJjsGPi8QAhPf5Nzg"
 
 
+# ================ MODEL====================
 model = ChatGoogleGenerativeAI(
     model = 'gemini-3.5-flash-lite',
-    google_api_key=GOOGLE_API_KEY
+    google_api_key = GOOGLE_API_KEY
 )
-# response = model.invoke("hello Buddy!")
+
+# response = model.invoke("Hello Buddy!")
 # response.content[-1]['text']
 
+
+# ======================TOOLS===============
 def search_latest_news_jobs(query):
-  '''This function helps to fetch latest
-    news or jobs related article using tavily'''
-  client=TavilyClient(
-      api_key=TAVILY_API_KEY)
+  """This function helps to fetch latest
+  news or jobs related article using
+  tavily"""
+
+  client = TavilyClient(
+      api_key = TAVILY_API_KEY)
   response = client.search(query)
   return response
 
 
-agent=create_agent(
-    model=model,
-    tools=[search_latest_news_jobs]
 
-)
+
+# Agent Creation
+agent = create_agent(
+    model = model,
+    tools = [search_latest_news_jobs])
+
 # agent
 
-def main_agent(agent,query):
-  '''this is main agent or leader agent orchestrate sub agent'''
-  prompt='''you are ai assistance and below given is a prompt ,your task is to give detailed prompt for this.
-  you are a professional resume generator where user will give their personal info you have to create detailed resume for students or professional one it must be with dynamic ui and ux ans,
-  with advance css professional designing
-  make sure to give output in html formnat only  no markdown allowed'''
-  response = agent.invoke({'messages':[{'role':'user','content':prompt}]})
+
+def main_agent(agent, query):
+  """This is main agent, or leader agent
+  orchestrate sub agents"""
+
+  # Giving prompt to create detailed prompt
+  # for code generation
+  prompt = """You are AI assistant and
+  below given is a prompt, your
+  task is to give detailed prompt for
+  this.
+  You are a professional Resume generator
+  where user will give their personal info,
+  you have to create detailed Resume
+  for students or professional one,
+  it must be with dynamic UI and UX and,
+  with advanced CSS Professional Designing
+  Make sure to give output in HTML format only
+  no markdowns allowed
+  """
+
+  response = agent.invoke({'messages':[{'role':'user',
+                                        'content':prompt}]})
   detailed_prompt = response['messages'][-1].content[-1]['text']
+
+  # SAVE PROMPT using File Handling
 
   with open('prompt.txt','w') as f:
     f.write(detailed_prompt)
-  return detailed_prompt
 
-  user_details='''below given is a user details generate resume based on that
-  if not given keep default resume python developer user details:{query}'''
-  final_prompt=prompt+detailed_prompt+user_details
-  response = agent.invoke({'messages':[{'role':'user','content':final_prompt}]})
-  return response['messages'][-1].content[-1]['text']
+  user_details = f"""Below Given is a user details
+  generate Resume based on that, if not
+  given keep: Default Resume: Python Developer
+  user details: {query}"""
+
+  final_prompt = prompt + detailed_prompt + user_details
+
+  # CODE GENERATION
+  response = agent.invoke({'messages':[{'role':'user',
+                                        'content':final_prompt}]})
+  code = response['messages'][-1].content[-1]['text']
 
   return code
 
@@ -66,18 +117,30 @@ def main_agent(agent,query):
 
 
 
-def get_jobs(agent,location='delhi',profile='data analyst, SDE'):
-  location='delhi'
-  profile='data analyst, SDE'
-  prompt="""Based on user given job proifile,using naukri,linkedin,indeed and other popular platform which are trusted and used to apply jobs
-  ,so results with job profile name location salary company name requirements.
-  show only jobs related to given{location}and{profile}
-  output must be in professsional html naukri theme carts with dynamic design """
-  response = agent.invoke({'messages':[{'role':'user','content':prompt}]})
-  return response['messages'][-1].content[-1]['text']
+# Fetch Latest Domain related Jobs using Tavily
+
+def get_jobs(agent,
+             Location = "Noida,Delhi",
+             Profile = "Data Analysts, AI Engineer"):
+  Location = "Noida,Delhi"
+  Profile = "Data Analysts, AI Engineer"
+
+  prompt = f"""Based on user given Job profile,
+  fetch latest jobs or job apply article
+  using Naukri, Linkedin, Indeed, or all popular
+  Job apply platforms, Show Results with
+  JOB PROFILE NAME, LOCATION, SALARY, COMPANY NAME,
+  SHOW jobs only related to given
+  {Location} and {Profile}. Output must be in
+  Professional HTML Naukri theme cards with Dynamic Design,
+  Show atleast Top 10-20 results with direct apply link"""
+
+
+  response = agent.invoke({'messages':[{'role':'user',
+                                          'content':prompt}]})
+  code = response['messages'][-1].content[-1]['text']
 
   return code
 
 # code = get_jobs(agent)
-#  from IPython import display as DISPLAY
 # DISPLAY.HTML(code)
