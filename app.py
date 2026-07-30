6b3d45af96a3b85c693f1e6b0858c3ad22e106ae
Siddharth <siddharthswami40@gmail.com>
2026-07-30 11:09:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ def get_jobs(agent,
 if st.button("Generate Resume"):
     with st.spinner("Agent Running"):
         code=main_agent(agent,user_info)
-        st.html(code,width='stretch" , unsafe_allow_javascript=True)
+        st.html(code,width="stretch" , unsafe_allow_javascript=True)
         st.divider()
         job_code = get_jobs(agent,location,profile)
         st.html(job_code, width="stretch",unsafe_allow_javascript=True)
