13f9e9a6fde72f6a052afbc8c4757f58366f8287
Siddharth <siddharthswami40@gmail.com>
2026-07-31 09:38:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ st.title("AI RESUME GENERATOR")
 st.write("""This app helps user to build customized Professional
 Resume with Latest Job apply links""")
 
-st.image("https://github.com/siddharthswami04/AI_Resume_Maker_and_job_Tracker/blob/main/BG.png")
+st.image("https://raw.githubusercontent.com/siddharthswami04/AI_Resume_Maker_and_job_Tracker/refs/heads/main/BG.jpg")
 st.sidebar.title("Fill Important Details")
-st.sidebar.image("https://github.com/siddharthswami04/AI_Resume_Maker_and_job_Tracker/blob/main/BG.png")
+st.sidebar.image("https://raw.githubusercontent.com/siddharthswami04/AI_Resume_Maker_and_job_Tracker/refs/heads/main/BG.jpg")
 
 
 
